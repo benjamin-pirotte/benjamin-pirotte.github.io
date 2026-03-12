738bab53d4165c8f66783529e9799922734ebd88
Benjamin Pirotte <benjaminpirotte@Benjamins-MacBook-Pro.local>
2026-03-12 13:31:41 +0100

    Updates

--- _next/static/chunks/app/layout-069ffc46b928d735.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},8427:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1290,23))}},e=>{e.O(0,[741,441,255,358],()=>e(e.s=8427)),_N_E=e.O()}]);
